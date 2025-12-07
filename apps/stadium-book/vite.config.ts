e5f623bd2652fa57418de2ca10f4e820fb996bd9
cyz <3391645389@qq.com>
2025-12-07 17:04:30 +0800

    chore: README

--- apps/stadium-book/vite.config.ts
+++ apps/stadium-book/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite"
+import { resolve } from "node:path"
 import vue from "@vitejs/plugin-vue"
-import vueDevTools from "vite-plugin-vue-devtools"
-import { resolve } from "path"
 import UnoCSS from "unocss/vite"
 // element-plus 自动引入
 import AutoImport from "unplugin-auto-import/vite"
-import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+
+import Components from "unplugin-vue-components/vite"
+import { defineConfig } from "vite"
+import vueDevTools from "vite-plugin-vue-devtools"
 
 // https://vite.dev/config/
 export default defineConfig({
